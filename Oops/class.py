a01376ae1b28d6926044a92e0e3b2906487dc05a
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-02-24 20:04:04 +0530

    oops in python

--- Oops/class.py
+++ Oops/class.py
@@ -36,4 +36,96 @@ person2 = Person("Waseem" , 21 , "Kupwara Lolab" , "Male" , "Computer Programmer
 person1.displayPersonInfo()
 print("\n")
 person2.displayPersonInfo()
+
+
+
+class Dog:
+    def __init__(self , name , breed , owner):
+        self.name = name
+        self.breed = breed
+        self.owner = owner
         
+    def bark(self , name) :
+        print(f"{self.name} is barking")
+        
+class Owner :
+    def __init__(self , name , address , contect_no):
+        self.name = name
+        self.address = address
+        self.contect = contect_no
+        
+
+owner1 = Owner("Joe" , "657-Tokyo" , "777-000-666")
+dog1 = Dog("Tommy" , "Male-dog" , owner1)
+    
+print(dog1.owner.contect)
+        
+        
+
+
+# another example
+
+# class User :
+#     def __init__(self , username , email , password):
+#         self.username = username
+#         self.email = email
+#         self.password = password
+    
+#     def greeting(self) :
+#         print(f"hi {self.username} how are you...")
+        
+
+# user1 = User("Rio" , "xyz123@gmail.com" , "01010011")
+# user1.greeting()
+
+
+
+# private and protected
+
+# class User :
+#     def __init__(self , username , email , password):
+#         self.username = username
+#         self._email = email
+#         self._password = password
+    
+#     def greeting(self) :
+#         print(f"hi {self.username} how are you...")
+        
+
+# user1 = User("Rio" , "xyz123@gmail.com" , "01010011")
+# print(user1._email)
+# print(user1._password)
+
+# user1._email = "rio123@gmai.com"
+# user1._password = "password" 
+
+# print(user1._email)
+# print(user1._password)
+
+
+
+
+# getters and setters
+
+class User :
+    def __init__(self , username , email , password):
+        self.username = username
+        self.__email = email
+        self.__password = password
+    
+    
+    def get_email(self):
+        return self.__email
+    
+    def set_email(self , newEmail) :
+        self.__email = newEmail
+        
+        
+    def greeting(self) :
+        print(f"hi {self.username} how are you...")
+        
+
+user1 = User("Rio" , "xyz123@gmail.com" , "01010011")
+print(user1.get_email())
+user1.set_email("hilal123@gmail.com")
+print(user1.get_email())
