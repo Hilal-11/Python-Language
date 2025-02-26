ee11d1310670f2baa3cbcd2567ca3f7a06d1effa
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-02-26 19:43:46 +0530

    encapsulation done

--- Oops/Encapsulation.py
+++ Oops/Encapsulation.py
@@ -16,26 +16,69 @@
 # car1.show_car_info()
 
 
-# privete data manbers and data functions
+# protected data manbers and data functions
 
-class Car :
-    def __init__(self , name , model , millage , licience):
-        self.name = name
-        self.model = model
-        self._millage = millage # protected
-        self._licience = licience    # protected
+# class Car :
+#     def __init__(self , name , model , millage , licience):
+#         self.name = name
+#         self.model = model
+#         self._millage = millage # protected
+#         self._licience = licience    # protected
         
-    def car_info(self) :
-        print(f"car is {self.name}, model {self.model}, millage {self._millage} and licience {self._licience}")
+#     def car_info(self) :
+#         print(f"car is {self.name}, model {self.model}, millage {self._millage} and licience {self._licience}")
         
     
-car1  = Car("Audi" , 2022 , "89kms" , False)
-car1.car_info()
+# car1  = Car("Audi" , 2022 , "89kms" , False)
+# car1.car_info()
 
-# try to manipulate 
-car1.name = "BMW"
-car1.model = "2025"
-car1.millage = "90kms" # not updated because the (protected) if we want to update/manipulate we must use backslash (_)
-car1.licience = True # not updated because the (protected) if we want to update/manipulate we must use backslash (_)
+# # try to manipulate 
+# car1.name = "BMW"
+# car1.model = "2025"
+# car1.millage = "90kms" # not updated because the (protected) if we want to update/manipulate we must use backslash (_)
+# car1.licience = True # not updated because the (protected) if we want to update/manipulate we must use backslash (_)
 
-car1.car_info()
+# car1.car_info()
+
+
+# privete data manbers and data functions
+
+class Students: 
+    def __init__(self , name , address , course , collage , contect , email , password , courseCode , regestration_no):
+            self.name = name
+            self.address = address
+            self.course = course
+            self.collage = collage
+            self.__contect = contect
+            self.__email = email
+            self.__password = password
+            self.__courseCode = courseCode
+            self.__regestration_no = regestration_no
+            
+    def studentInfo(self) :
+        print(f'''
+            name : {self.name}
+            address : {self.address}
+            course : {self.course}
+            collage : {self.collage}
+            ''')
+    def student_personal_info(self):
+        print(f'''
+            contect : {self.__contect}
+            email: {self.__email}
+            password :{self.__password}
+            course code: {self.__courseCode}
+            regestration: {self.__regestration_no} 
+            ''')
+    def set_student_personal_info(self , contect , email , password , coursecode , regNo):
+        self.__contect = contect
+        self.__email = email
+        self.__password = password
+        self.__courseCode = coursecode
+        self.__regestration_no = regNo
+        
+        
+
+student1  = Students("Hilal", "lolab" , "BCA" , "GDC-Fakarchand" , "772-282-828" , "hilalahmadcodedev123@gmail.com" , "01010011" , "bca_fakarchand0101" , "001101063")
+student1.studentInfo();
+student1.student_personal_info()
