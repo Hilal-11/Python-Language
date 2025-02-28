c16300fbe589199813866d2ecdc8f388a1f3fccd
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-02-28 21:23:48 +0530

    first python game random guess number

---
+++ Games/GuessRandom.py
@@ -0,0 +1,17 @@
+
+import random
+
+def guess(n) :
+    random_number = random.randint(1 , n)
+    guess = 0
+    while (guess != random_number) :
+        guess = int(input(f"Guess the random between 1 to {n} :-"))
+        if(guess > random_number) :
+            print("sorry guess again, guess is too high")
+        elif (guess < random_number) :
+            print("sorry guess again, guess is too low")
+        else :
+            print("hey 😃 congratulation you guess the correct number")
+        
+
+guess(10)
