648d090d094b0277dba22f5afa0ecc5ec0ad29ce
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-03-04 22:03:50 +0530

    map higher order function

--- Map.py
+++ Map.py
@@ -16,3 +16,72 @@ def double(val) :
 
 result = list(map(double , l))
 print(result)
+
+
+# example first of map function
+
+elements = ["Apple" , "Banana" , "Orange" , "Grapes"]
+print(elements)
+
+def concate_with_fruit(elements) :
+    return "Fruit "+ elements
+result = list(map(concate_with_fruit , elements))
+print(result)
+
+print("\n")
+print("\n")
+
+# example second of map function
+    # conseder the list of integers return only even elements from that list using map()
+elms1 = [2, 5 , 7 , 9 , 8 , 3 , 4 , 12 , 16]
+def evenElements(elements) :
+    if(elements % 2 == 0):
+        return elements
+    else:
+        return 'odd'
+evenList = list(map(evenElements , elms1))
+print(evenList)
+    # conseder the list of integers return only odd elements from that list using map()
+elms2= [2, 5 , 7 , 9 , 8 , 3 , 4 , 12 , 16]
+def oddElements(elements) :
+    if (elements % 2 != 0):
+        return elements
+    else :
+        return 'even'
+oddList = list(map(oddElements , elms2))
+print(oddList)
+
+
+
+
+# elms1 = [2, 5 , 7 , 9 , 8 , 3 , 4 , 12 , 16]
+# def evenElements(elements) :
+#     lst = []
+#     if(elements % 2 == 0):
+#         lst.append(elements)
+#     return lst    
+# evenList = list(map(evenElements , elms1))
+# print(evenList)
+#     # conseder the list of integers return only odd elements from that list using map()
+# elms2= [2, 5 , 7 , 9 , 8 , 3 , 4 , 12 , 16]
+# def oddElements(elements) :
+#     if (elements % 2 != 0):
+#         return elements
+# oddList = list(map(oddElements , elms2))
+# print(oddList)
+
+
+
+
+    # this recomeded only with filter function
+    # this recomeded only with filter function
+    
+# elms1 = [2, 5 , 7 , 9 , 8 , 3 , 4 , 12 , 16]
+
+# evenList = list(filter(lambda elms1 : elms1 % 2 == 0 , elms1))
+# print(evenList)
+#     # conseder the list of integers return only odd elements from that list using map()
+# elms2= [2, 5 , 7 , 9 , 8 , 3 , 4 , 12 , 16]
+
+# oddList = list(filter(lambda elms2: elms2 % 2 != 0 , elms2))
+# print(oddList)
