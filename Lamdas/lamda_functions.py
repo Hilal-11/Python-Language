55560cdfaf68910c0f602d7d64023295508d2322
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-03-06 14:32:50 +0530

    done

--- Lamdas/lamda_functions.py
+++ Lamdas/lamda_functions.py
@@ -13,3 +13,24 @@ mul = lambda x , y , z : x*y*z
 print(mul(10 , 6 , 2))
 
 
+# sum = lambda **args : 
+
+# students = [("Hilal" , 90), ("waseem" , 89) , ("Aadil", 80)]
+# print(students)
+# for iter in students :
+#         for i in iter :
+#             print(i)
+            
+
+students = [("Hilal" , 60), ("waseem" , 89) , ("Aadil", 70)]
+# students.sort()
+# print(students)
+
+def sort_students(data) :
+    return data[1]
+
+students.sort(key=sort_students)
+print(students)
+
+students.sort(key=sort_students , reverse=True)
+print(students)
