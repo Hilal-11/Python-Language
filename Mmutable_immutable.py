809f4e0e0988b4312cc825218e3c8eabf16b17ab
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-03-06 23:50:04 +0530

    mutable-immutable

---
+++ Mmutable_immutable.py
@@ -0,0 +1,34 @@
+
+# mutable and immutable 
+
+
+# username = "Hilal"
+# username
+# 'Hilal'
+# username = "Aadil"
+# username
+# 'Aadil'
+
+
+
+
+# >>> x = 10
+# >>> y = x
+# >>> x
+# 10
+# >>> y
+# 10
+# >>> x = 15
+# >>> y
+# 10
+# >>> x
+# 15
+# >>> 
+
+x = 10
+y = x
+print(x , y)
+x = 20
+# y value should changed to 20
+print(y)
+print(x)
