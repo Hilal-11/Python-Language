1439ac05fdc90b9daf6ed218e5334cf20f7d872a
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-03-07 00:06:29 +0530

    mutable immutable

--- Mmutable_immutable.py
+++ Mmutable_immutable.py
@@ -29,6 +29,24 @@ x = 10
 y = x
 print(x , y)
 x = 20
+y = x
 # y value should changed to 20
 print(y)
 print(x)
+
+
+    # Mutable               
+        # list 
+        # set
+        # disctionary
+        # bytearray
+        # array
+
+    # immutable
+        # Integers
+        #Float-points numbers
+        # boolean
+        # strings
+        # typles
+        # frozen set
+        # byyes
