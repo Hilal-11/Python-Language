b7e0a53b222f0b546771b84ece27c234fbcf8aa4
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-03-06 14:16:37 +0530

    lamdas

---
+++ Lamdas/lamda_functions.py
@@ -0,0 +1,15 @@
+
+# Lamda functions in python 
+
+# lambda arguments : expression
+    # a lamda function is a small anonomous function , that takes number of argumants and have only 
+    # single/one expression
+# lambda x , y : x + y
+
+add = lambda x , y : x + y
+print(add(10 , 20))
+
+mul = lambda x , y , z : x*y*z
+print(mul(10 , 6 , 2))
+
+
