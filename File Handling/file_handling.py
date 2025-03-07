66062575664af4e5e95078aa49c1d390317cea29
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-03-07 14:09:50 +0530

    file handling

---
+++ File Handling/file_handling.py
@@ -0,0 +1,16 @@
+print("File Handling in python")
+
+
+# # f = open("demofile.txt")
+# try: 
+#     f = open("demofile.txt", "r") 
+#     print(f.read())
+# except FileNotFoundError :
+#     print("the file 'demofile.txt' does not exist")
+
+
+# try :
+#     with open("demofile.txt" , "r") as f:
+#         print(f.read())
+# except FileNotFoundError :
+#     print("the file 'demofile.txt' does not exists")
