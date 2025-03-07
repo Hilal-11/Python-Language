8fd63d9a936d98d38f049e95b0a0533472c51aef
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-03-07 14:16:08 +0530

    file handling practive

--- File Handling/file_handling.py
+++ File Handling/file_handling.py
@@ -14,3 +14,23 @@ print("File Handling in python")
 #         print(f.read())
 # except FileNotFoundError :
 #     print("the file 'demofile.txt' does not exists")
+
+
+
+# create the file and write some content on it and open to read
+with open("demo.txt" , "w") as f:
+    f.write("Hello! Welcome to demofile.txt This file is for testing purposes. Good Luck!")
+# now open this file
+try :
+    with open("demo.txt" , "r") as f:
+        print(f.read())
+except FileNotFoundError:
+    print("The file 'demo.txt' does't exists")
+    
+    
+print("\n\n")
+try :
+    with open("demofile.txt" , "r") as f:
+        print(f.read()) 
+except FileNotFoundError :
+    print("The file 'demofile.txt' does't exists")
