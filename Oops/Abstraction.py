5bc26a0c31499b7f6f225650285da45207ba48a3
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-03-08 18:21:54 +0530

    oops abstracion

---
+++ Oops/Abstraction.py
@@ -0,0 +1,20 @@
+
+
+print("Oops -> Abstraction")
+
+class Animal:
+    def make_sound(self):
+        pass
+
+class Dog(Animal):
+    def make_sound(self):
+        return "Bark"
+class Cat(Animal):
+    def make_sound(self):
+        return "Meow"
+    
+dog = Dog()
+cat = Cat()
+
+print(dog.make_sound())
+print(cat.make_sound())
