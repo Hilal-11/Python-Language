3beb26bb8e48794a1004d4a6f38d9261c0454835
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-03-10 22:18:09 +0530

    decorators in cpp

---
+++ Decorators/decorator.py
@@ -0,0 +1,22 @@
+import time
+
+def timer(fx) :
+    def wrapper(*args , **kwargs) :
+        start = time.time()
+        result = fx(*args , **kwargs)
+        end = time.time()
+        print(f"{fx.__name__} runs in {end-start}")
+        return result
+    return wrapper
+
+@timer
+def example_function(n) :
+    time.sleep(n)
+    
+example_function(2)   
+
+@timer
+def find_factorial(n):
+    time.sleep(n)
+
+find_factorial(4) 
