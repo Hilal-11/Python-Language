00737611ed041fd857f4167d41b0717750a19bd3
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-03-11 19:17:39 +0530

    generators PY

---
+++ Generators/Generators2.py
@@ -0,0 +1,42 @@
+
+# def generateList(n) :
+#     items = []
+#     for x in range(n) :
+#         items.append(x)
+#     return items
+
+# listItems = generateList(100)
+# print(listItems)
+
+
+
+def list_n(n) :
+    num = 0
+    numsList = []
+    while num < n:
+        numsList.append(num)
+        num += 1
+    return numsList
+myList = list_n(200)
+print(myList)
+
+
+# a generator that yields items instead of returning a list
+def firstn(n):
+    num = 0
+    while num < n:
+        yield num
+        num += 1
+ 
+sum_of_first_n = firstn(1000000)
+print(next(sum_of_first_n))
+print(next(sum_of_first_n))
+print(next(sum_of_first_n))
+print(next(sum_of_first_n))
+print(next(sum_of_first_n))
+print(next(sum_of_first_n))
+print(next(sum_of_first_n))
+print(next(sum_of_first_n))
+print(next(sum_of_first_n))
+print(next(sum_of_first_n))
+print(next(sum_of_first_n))
