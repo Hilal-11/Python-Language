6c83a963b5b1ee4e3657d31c1f4663c8de71f503
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-03-15 18:34:10 +0530

    operations

--- Games/Calculator_CLI.py
+++ Games/Calculator_CLI.py
@@ -6,16 +6,23 @@ def perform_addition(x , y) :
 def perform_subtraction(x , y):
     result = x - y
     print(f"Subtraction of {x} - {y} = " , result)
+    
+def perform_multiplication(x , y):
+    result = x * y
+    print(f"Multiplication of {x} * {y} = " , result)
+    
 def main() :
     
     while True:
         
         print("\n Python CLI Calculator  | Choose an option to perforn calculations \n")
+        print("*" * 100)
         print("1:- Addition operation")
         print("2:- Subtraction operation")
         print("3:- Multiplication operation")
         print("4:- Division operation")
         print("5:- Comparision operation")
+        print("*" * 100)
         
         choice = input("Enter your choice :- ")  
         
@@ -23,10 +30,15 @@ def main() :
             num1 = int(input("Enter first number :- "))
             num2 = int(input("Enter second number :- "))
             perform_addition(num1 , num2)
-        if(choice == '2'):
+        elif(choice == '2'):
             num1 = int(input("Enter first number :- "))
             num2 = int(input("Enter second number :- "))
             perform_subtraction(num1 , num2)
+        elif(choice == '3'): 
+            num1 = int(input("Enter first number :- "))
+            num2 = int(input("Enter second number :- "))
+            perform_multiplication(num1 , num2)
+           
             
                   
         
