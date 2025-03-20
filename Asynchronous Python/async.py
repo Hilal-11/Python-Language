193d6938ddb4b3da9aad307dd6be2c6472b8b6ef
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-03-20 22:55:24 +0530

    async .py

--- Asynchronous Python/async.py
+++ Asynchronous Python/async.py
@@ -1,15 +1,34 @@
 import asyncio
 
-async def greeting() :
-    print("execution start")
-    await asyncio.sleep(2)
-    print("Asynchronous execution is not a multi-threading or multi-tasking , it is non-blocking code")
-    await asyncio.sleep(3)
-    print("execution end")
-    await asyncio.sleep(1)
-    print('Hello')
+# async def greeting() :
+#     print("execution start")
+#     await asyncio.sleep(2)
+#     print("Asynchronous execution is not a multi-threading or multi-tasking , it is non-blocking code")
+#     await asyncio.sleep(3)
+#     print("execution end")
+#     await asyncio.sleep(1)
+#     print('Hello')
     
 
 
-# asyncio.run(greeting()) 
-asyncio.run(greeting())
+# # asyncio.run(greeting()) 
+# asyncio.run(greeting())
+
+
+async def function1() :
+    print("one")
+    await asyncio.sleep(1)
+    print("two")
+    await asyncio.sleep(1)
+    await function2()
+    await asyncio.sleep(1)
+    print("five")
+    
+    
+async def function2() :
+    print("three")
+    await asyncio.sleep(1)
+    print("four")
+    
+    
+asyncio.run(function1())
