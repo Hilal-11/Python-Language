6c01f0b81716189546a9a34cc9d50759eecb4b65
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-03-20 23:03:18 +0530

    async  =pt

--- Asynchronous Python/async.py
+++ Asynchronous Python/async.py
@@ -15,20 +15,35 @@ import asyncio
 # asyncio.run(greeting())
 
 
+# async def function1() :
+#     print("one")
+#     await asyncio.sleep(1)
+#     print("two")
+#     await asyncio.sleep(1)
+#     await function2()
+#     await asyncio.sleep(1)
+#     print("five")
+    
+    
+# async def function2() :
+#     print("three")
+#     await asyncio.sleep(1)
+#     print("four")
+    
+    
+# asyncio.run(function1())
+
 async def function1() :
+    task = asyncio.create_task(function2())
     print("one")
     await asyncio.sleep(1)
-    print("two")
-    await asyncio.sleep(1)
-    await function2()
+    print("four")
     await asyncio.sleep(1)
     print("five")
-    
-    
+
 async def function2() :
-    print("three")
+    print("Two")
     await asyncio.sleep(1)
-    print("four")
-    
+    print("three")
     
 asyncio.run(function1())
