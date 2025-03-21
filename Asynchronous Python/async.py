e32d3be68809a5d8ac7157c2a423bce1d11868b2
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-03-21 22:46:52 +0530

    synchronous execution

--- Asynchronous Python/async.py
+++ Asynchronous Python/async.py
@@ -1,5 +1,5 @@
 import asyncio
-
+import time
 # async def greeting() :
 #     print("execution start")
 #     await asyncio.sleep(2)
@@ -33,17 +33,36 @@ import asyncio
     
 # asyncio.run(function1())
 
-async def function1() :
-    task = asyncio.create_task(function2())
-    print("one")
-    await asyncio.sleep(1)
-    print("four")
-    await asyncio.sleep(1)
-    print("five")
+# async def function1() :
+#     task = asyncio.create_task(function2())
+#     print("one")
+#     await asyncio.sleep(1)
+#     print("four")
+#     await asyncio.sleep(1)
+#     print("five")
 
-async def function2() :
-    print("Two")
-    await asyncio.sleep(1)
-    print("three")
+# async def function2() :
+#     print("Two")
+#     await asyncio.sleep(1)
+#     print("three")
     
-asyncio.run(function1())
+# asyncio.run(function1())
+
+
+
+# synchronous manner execution
+def function1() :
+    time.sleep(2)
+    print("Function 1 is executed")
+
+def function2() :
+    time.sleep(1)
+    print("Function 2 is executed")
+
+def function3() :
+    time.sleep(3)
+    print("Function 3 is executed")
+    
+function1()
+function2()
+function3()
