c300fa3fe8d308083bdf569d2156101633fa7f7d
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-03-22 14:51:48 +0530

    python get requests

--- Python Network Requests/get.py
+++ Python Network Requests/get.py
@@ -1,2 +1,13 @@
 import requests
 
+# x = requests.get('https://w3schools.com/python/demopage.htm')
+# print(x.text)
+
+URL = "https://api.sampleapis.com/jokes/goodJokes"
+
+response = requests.get(URL)
+if response.status_code == 200 :
+    print("Retreive data from server successfully")
+    print(response.json())
+else:
+    print("Failed to get data from server")
