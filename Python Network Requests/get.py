6e435fd47cfa8560af044e3840df29f95937dbc0
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-03-22 15:10:02 +0530

    get the and write it on .json file

--- Python Network Requests/get.py
+++ Python Network Requests/get.py
@@ -1,13 +1,22 @@
 import requests
+import json
 
 # x = requests.get('https://w3schools.com/python/demopage.htm')
 # print(x.text)
 
+print("start")
 URL = "https://api.sampleapis.com/jokes/goodJokes"
 
 response = requests.get(URL)
 if response.status_code == 200 :
     print("Retreive data from server successfully")
+    try:
+        with open("jokes.json" , "w") as f:
+            json.dump(response.json() , f , indent=4)
+    except:
+        print("Failed to fetch data form api and generate a json file")
     print(response.json())
 else:
-    print("Failed to get data from server")
+    print("Failed to get data from server") 
+    
+print("end")
