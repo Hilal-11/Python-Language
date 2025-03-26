622ad62b4e2ec98b068fc05eadd5ff65e3864ec2
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-03-26 08:18:32 +0530

    range

---
+++ Collage Syllabus Python/range.py
@@ -0,0 +1,29 @@
+
+
+#       range(start, stop, step)
+
+
+# n = 10
+# for i in range(n) :
+#     print(f" {i}",end=" ")
+
+start = 1
+stop = 20
+step = 2
+
+# for i in range(start , stop , 1):
+#     print(i,end=" ")
+
+
+# for i in range(start , stop , 1):
+#     print(i,end=" ")
+
+
+# for i in range(0 , 100+1 , 5):
+#     print(i,end=" ")
+    
+    
+# data = [55, 60, 65, 70 ,75, 80, 85, 90, 95, 100]
+
+# for i in range(4):
+#     print(data[i])
